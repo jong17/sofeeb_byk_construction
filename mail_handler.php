8f0b38561e238a77e7cb44e63cdc8b37fce5efa9
jong17 <faisol.lateh@gmail..com>
2021-05-30 12:08:27 +0700

    fix alert and format utf8

--- mail_handler.php
+++ mail_handler.php
@@ -1,20 +1,23 @@
 <?php 
-    header('Location: https://cebyk.com');
 // if(isset($_POST['submit'])){
     $to = "Cons&Eng@cebyk.com"; // this is your Email address
     $from = "noreply@cebyk.com"; // this is the sender's Email address
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $subject = "Form submission";
+    $subject = "ขอใบเสนอราคา";
     // $subject2 = "Copy of your form submission";
-    $message = $name . " " . $email . " wrote the following:" . "\n\n" . $_POST['msg'];
+    $message = "คุณ " . $name . "\n\n อีเมล " . $email . " \n\nรายละเอียดเพิ่มเติม:\n\n" . $_POST['msg'];
     // $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
-    $headers = "From:" . $from;
+    $headers = 'Content-Type: text/plain; charset=utf-8' . "\r\n";
+    $headers .= "From:" . $from;
     // $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo '<script>alert("Mail Sent. Thank you " . $name . ", we will contact you shortly.")</script>';
+    echo "<script language=\"JavaScript\">";
+    echo "alert('Mail Sent. Thank you, we will contact you shortly.');";
+    echo "window.location=\"https://cebyk.com\";";
+    echo "</script>";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // }
     exit();
